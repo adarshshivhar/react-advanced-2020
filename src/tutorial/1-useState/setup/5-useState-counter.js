2737a9edbcbfa047c127d7fc62457f23bb2afbf4
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-10-17 14:31:32 +0530

    Simple Counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const reset = () => {
+    setValue(0);
+  }
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button
+          type='button'
+          className='btn'
+          onClick={() => setValue(value - 1)}
+        >
+          decrease
+        </button>
+        <button type='button' className='btn' onClick={reset}>
+          reset
+        </button>
+        <button
+          type='button'
+          className='btn'
+          onClick={() => setValue(value + 1)}
+        >
+          increase
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
